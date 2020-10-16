d4e6e1bdccc9fb3bb08d7cbf1b0b2c03b8089170
usoltsevjr <65492119+usoltsevjr@users.noreply.github.com>
2020-10-16 10:41:23 +0300

    Update AuthTest.java

--- src/test/java/ru/netology/web/AuthTest.java
+++ src/test/java/ru/netology/web/AuthTest.java
@@ -3,17 +3,22 @@ package ru.netology.web;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 import static org.openqa.selenium.By.cssSelector;
 
 public class AuthTest {
+    
+    @BeforeEach
+    void setUpAll() {
+        open("http://localhost:9999");
+    }
 
     @Test
     void shouldSubmitRequestIfUserIsActive() {
         Registration user = DataGenerator.generateNewActiveUser();
-        open("http://localhost:9999");
         SelenideElement form = $("[action]");
         form.$(cssSelector("[name=login]")).sendKeys(user.getLogin());
         form.$(cssSelector("[name=password]")).sendKeys(user.getPassword());
@@ -24,7 +29,6 @@ public class AuthTest {
     @Test
     void shouldNotSubmitRequestIfStatusIsBlocked() {
         Registration user = DataGenerator.generateNewBlockedUser();
-        open("http://localhost:9999");
         SelenideElement form = $("[action]");
         form.$(cssSelector("[name=login]")).sendKeys(user.getLogin());
         form.$(cssSelector("[name=password]")).sendKeys(user.getPassword());
@@ -35,7 +39,6 @@ public class AuthTest {
     @Test
     void shouldNotSubmitRequestIfLoginInvalid() {
         Registration user = DataGenerator.generateNewUserWithInvalidLogin();
-        open("http://localhost:9999");
         SelenideElement form = $("[action]");
         form.$(cssSelector("[name=login]")).sendKeys(user.getLogin());
         form.$(cssSelector("[name=password]")).sendKeys(user.getPassword());
@@ -46,7 +49,6 @@ public class AuthTest {
     @Test
     void shouldNotSubmitRequestIfPasswordInvalid() {
         Registration user = DataGenerator.generateNewUserWithInvalidPassword();
-        open("http://localhost:9999");
         SelenideElement form = $("[action]");
         form.$(cssSelector("[name=login]")).sendKeys(user.getLogin());
         form.$(cssSelector("[name=password]")).sendKeys(user.getPassword());
